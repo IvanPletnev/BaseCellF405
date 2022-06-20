0fe6052c0802f28836776e487ae9cc4cd5671b75
IvanPletnev <ivan@tensib.ru>
2022-06-20 12:37:56 +0300

    1.06.074 Uart error int handler added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1203,6 +1203,20 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	}
 
 }
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+	if (__HAL_UART_GET_FLAG(huart, UART_FLAG_ORE) != RESET){
+		if (__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE) == RESET){
+			huart->Instance->DR;
+		}
+	}
+	if (huart->Instance == USART1){
+		HAL_UART_Receive_DMA(&huart1, raspRxBuf, RASP_RX_BUF_SIZE);
+	} else if (huart->Instance == USART6){
+		HAL_UART_Receive_DMA(&huart6, currentVoltageRxBuf, CV_RX_BUF_SIZE);
+	}
+}
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -1283,7 +1297,7 @@ void accelTask(void const * argument)
 	for (;;) {
 		evt = osSignalWait(0x01, osWaitForever);
 		if (evt.status == osEventSignal) {
-			if (osMutexWait(I2C2MutexHandle, 50) == osOK){
+			if (osMutexWait(I2C2MutexHandle, 5) == osOK){
 				intSource = getInterruptSource();
 				ADXL345_Read(buffer);
 				osMutexRelease(I2C2MutexHandle);
@@ -1670,18 +1684,18 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	/*------------------------------------------------------------------------------------------*/
 
 	if (htim->Instance == TIM8) { //Сюда мы попадаем, если 200ms нет ответа от SourceSelector
-		HAL_TIM_Base_Stop_IT(&htim8);
-		__HAL_TIM_CLEAR_IT(&htim8, TIM_IT_UPDATE);
-		__HAL_TIM_SET_COUNTER(&htim8, 0);
-		__HAL_UART_DISABLE_IT(&huart6, UART_IT_IDLE);
-		__HAL_UART_CLEAR_IDLEFLAG(&huart6);
-		HAL_UART_AbortTransmit(&huart6);
-		HAL_UART_DMAStop(&huart6);
-		HAL_UART_AbortReceive(&huart6);
-		HAL_UART_DeInit(&huart6);
-		MX_USART6_UART_Init();
-		__HAL_UART_ENABLE_IT(&huart6, UART_IT_IDLE);
-		HAL_UART_Receive_DMA(&huart6, currentVoltageRxBuf, CV_RX_BUF_SIZE);
+//		HAL_TIM_Base_Stop_IT(&htim8);
+//		__HAL_TIM_CLEAR_IT(&htim8, TIM_IT_UPDATE);
+//		__HAL_TIM_SET_COUNTER(&htim8, 0);
+//		__HAL_UART_DISABLE_IT(&huart6, UART_IT_IDLE);
+//		__HAL_UART_CLEAR_IDLEFLAG(&huart6);
+//		HAL_UART_AbortTransmit(&huart6);
+//		HAL_UART_DMAStop(&huart6);
+//		HAL_UART_AbortReceive(&huart6);
+//		HAL_UART_DeInit(&huart6);
+//		MX_USART6_UART_Init();
+//		__HAL_UART_ENABLE_IT(&huart6, UART_IT_IDLE);
+//		HAL_UART_Receive_DMA(&huart6, currentVoltageRxBuf, CV_RX_BUF_SIZE);
 		for (cvCounter = 0; cvCounter < CV_RX_BUF_SIZE; cvCounter++) {
 			currentVoltageRxBuf[cvCounter] = 0;
 		}
